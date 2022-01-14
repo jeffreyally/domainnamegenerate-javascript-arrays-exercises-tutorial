671c6c15d01bf4fd93e3ac0a89dbf0025d78c0fc
jeffreyally <jeffreyally@gmail.com>
2022-01-14 22:54:40 +0000

    21.3 filter array of strings

--- exercises/21.3-Filter-list/app.js
+++ exercises/21.3-Filter-list/app.js
@@ -1,5 +1,14 @@
 let names = ['Liam','Emma','Noah','Olivia','William','Ava','James','Isabella','Logan','Sophia','Benjamin','Mia','Mason','Charlotte','Elijah','Amelia','Oliver','Evelyn','Jacob','Abigail','Lucas','Harper','Michael','Emily','Alexander','Elizabeth','Ethan','Avery','Daniel','Sofia','Matthew','Ella','Aiden','Madison','Henry','Scarlett','Joseph','Victoria','Jackson','Aria','Samuel','Grace','Sebastian','Chloe','David','Camila','Carter','Penelope','Wyatt','Riley'];
 
-//declare your function here
+let filterByName = (array,string) =>{
+    newarray = array.filter((elements) => {
+
+        return elements.includes(string)
+    })
+
+    return newarray
+
+}
 
 console.log(filterByName(names, 'am'));
+
