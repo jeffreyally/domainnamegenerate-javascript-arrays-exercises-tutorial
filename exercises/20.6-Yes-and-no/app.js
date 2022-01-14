ab59963d7aaba5e3158b4acfb03caaf35473189c
jeffreyally <jeffreyally@gmail.com>
2022-01-14 22:05:01 +0000

    20.6 y and n my solution

--- exercises/20.6-Yes-and-no/app.js
+++ exercises/20.6-Yes-and-no/app.js
@@ -1,3 +1,16 @@
 let theBools = [0,1,0,0,1,1,1,0,0,1,0,1,1,0,0,0,0,0,0,0,0,1,0,0,0,0,1];
 
-//your code here
+wikiwoko = theBools.map((element) =>{
+
+    if(element == 1){
+        return 'wiki'
+
+    }
+
+    else{
+        return 'woko'
+    }
+
+});
+
+console.log(wikiwoko)
