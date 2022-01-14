7fe70a52112c43ac683c4b66c58a4de6c46fe97a
jeffreyally <jeffreyally@gmail.com>
2022-01-14 21:16:51 +0000

    14-Divide and conquer

--- exercises/14-Divide-and-Conquer/app.js
+++ exercises/14-Divide-and-Conquer/app.js
@@ -1,6 +1,28 @@
 let list_of_numbers = [4, 80, 85, 59, 37,25, 5, 64, 66,	81,20, 64, 41, 22, 76,76, 55, 96, 2, 68];
 
-// your code here
+let mergeTwoList = (arrayofnumbers) =>{
+
+    odd = [];
+    even = [];
+    arrayofnumbers.forEach((element) => {
+
+        if(element%2==0){
+            even.push(element)
+
+        }
+        else{
+            odd.push(element)
+        }
+
+    
+
+    });
+    for(i=0;i<even.length;i++){
+        odd.push(even[i])
+    }
+    return odd
+
+}
 
 
 console.log(mergeTwoList(list_of_numbers))
