699668d8c7d274264102c8482bd50b186a320162
jeffreyally <jeffreyally@gmail.com>
2022-01-14 22:09:15 +0000

    21 filter an array my solution

--- exercises/21-Filter-an-array/app.js
+++ exercises/21-Filter-an-array/app.js
@@ -1,5 +1,5 @@
-let allNames = ["Romario","Boby","Roosevelt","Emiliy", "Michael", "Greta", "Patricia", "Danzalee"];
-
-//your code here
+let allNames = ["romario","Boby","Roosevelt","Emiliy", "Michael", "Greta", "Patricia", "Danzalee"];
+//changed Romario to lowercase and it worked as well
+resultingNames = allNames.filter(element =>  element.toUpperCase().startsWith('R'))
 
 console.log(resultingNames);
